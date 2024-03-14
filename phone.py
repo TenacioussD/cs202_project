8b13d3adc3ea2a127d6e76e9b93c811f68c91830
TenaciousD <tanisha.dyer@gmail.com>
2024-03-14 19:57:54 +1300

    Playing around

--- phone.py
+++ phone.py
@@ -5,5 +5,4 @@ def phonebook_lookup(filename):
             for line in f:
 raw_words = line.split()   phonebook_dict[raw_words[0].strip(unwanted_chars)] = raw_words[1].strip(unwanted_chars)
       return(phonebook_dict)
-
 phonebook=phonebook_lookup("phonebook.txt")
